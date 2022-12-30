b97f422a02258375b46113f2f89e6441d1bbde35
Richard Hsu <45455105+bung8833@users.noreply.github.com>
2022-12-30 21:05:37 +0800

    Add OOP HW

---
+++ IsMarketHoursUtility.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketHoursUtility
+{
+    public class IsMarketHoursUtility
+    {
+        public static bool IsMarketHours(DateTime dt)
+        {
+            if (dt.DayOfWeek >= DayOfWeek.Monday
+             && dt.DayOfWeek <= DayOfWeek.Friday)
+            {
+                if (dt.TimeOfDay >= new TimeSpan(9, 0, 0)
+                    && dt.TimeOfDay < new TimeSpan(13, 30, 0))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ ToLowerCaseUtility.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LowerCase.Utility
+{
+    public class ToLowerCaseUtility
+    {
+        public static string ToLowerCase(string str)
+        {
+            if (String.IsNullOrEmpty(str))
+                return str;
+            
+
+            string strLower = str.ToLower();
+            string result = String.Empty;
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                // Lower Case
+                if (str[i] == strLower[i])
+                {
+                    result += strLower[i];
+                }
+                else // Upper Case
+                {
+                    result += "_" + strLower[i];
+                }
+            }
+
+            // do not start with "_"
+            if (result.StartsWith("_"))
+            {
+                return result.Substring(1);
+            }
+            else
+            {
+                return result;
+            }
+        }
+    }
+}
